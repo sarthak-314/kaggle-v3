ef0c7b9e57d60189d867eb84dc3ba15210935924
Sarthak <sarthak.bhatt314@gmail.com>
2021-06-28 00:57:16 +0530

    lazy push

--- kaggle/IMP.md
+++
@@ -1,5 +0,0 @@
-function lazygit() {
-    git add .
-    git commit -m "lazy push"
-    git push
-}
---
+++ kaggle/scripts.py
@@ -0,0 +1,23 @@
+"""
+SYNC NOTEBOOK WITH VS CODE 
+--------------------------
+# Sync Notebook code with Local Scripts
+REPO = 'https://github.com/sarthak-314/kaggle-v3'
+!git clone {REPO}
+%cd /content/kaggle-v3/
+!git pull 
+
+# Run the Startup Script for the Competition
+%run /content/kaggle-v3/covid/startup.py
+"""
+
+"""
+LAZYGIT
+-------
+function lazygit() {
+    git add .
+    git commit -m "lazy push"
+    git push
+}
+"""
+
