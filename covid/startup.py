40fb3e482e5c2ae987f6014e3038432867bd82c6
Sarthak <sarthak.bhatt314@gmail.com>
2021-06-30 02:26:45 +0530

    lazy push

--- covid/startup.py
+++ covid/startup.py
@@ -8,7 +8,6 @@ S3_PATH = 's3://siim-covid19-detection/'
 
 
 def download_from_s3(dataset_name, output_path):
-    print('dataset_name: ', dataset_name)
-    print(': ', output_path)
+    output_path = str(output_path)
     bucket_path = S3_PATH + dataset_name
     subprocess.run(['aws', 's3', 'cp',  bucket_path, output_path, '--recursive'])
