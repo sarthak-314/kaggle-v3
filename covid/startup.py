5b7b8c06b58b81951c3cb3b7084573082708035e
Sarthak <sarthak.bhatt314@gmail.com>
2021-06-30 02:22:47 +0530

    lazy push

--- covid/startup.py
+++ covid/startup.py
@@ -4,7 +4,7 @@ from utils.startup import *
 COMP_NAME = 'siim-covid19-detection'
 COMP_INPUT_PATH = Path('/kaggle/input/siim-covid19-detection')
 
-S3_PATH = 's3://siim-covid19-detection'
+S3_PATH = 's3://siim-covid19-detection/'
 
 
 def download_from_s3(dataset_name, output_path):
