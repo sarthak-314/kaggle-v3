2aa9653b59b291df3f75f0862b93b59f209941a2
Sarthak <sarthak.bhatt314@gmail.com>
2021-06-30 07:52:33 +0530

    lazy push

--- covid/startup.py
+++ covid/startup.py
@@ -11,3 +11,16 @@ def download_from_s3(dataset_name, output_path):
     output_path = str(output_path)
     bucket_path = S3_PATH + dataset_name
     subprocess.run(['aws', 's3', 'cp', bucket_path, output_path, '--recursive'], stdout=subprocess.PIPE)
+    
+# TODO: Build dataset_dicts for object detection
+DATASETS = [
+    'dataframes', # Dataframes for the competition
+    'study_level_classification_full', # Full sized images for study level classification
+    'image_level_classification_full', # Full sized images for image level classification
+    'raddar_study_level_full', # Raddar's external dataset for study level classification
+    'object_detection', # Object detection data from competition in coco format
+]
+
+import covid.datasets.dataframes
+
+# Read train, valid dataframes for fold 0 for quick iteration
