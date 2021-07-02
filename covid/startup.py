722a48e4fc18c5358b3208ca1c44e20da8200c2d
Sarthak <sarthak.bhatt314@gmail.com>
2021-07-02 20:05:22 +0530

    lazy push

--- covid/startup.py
+++ covid/startup.py
@@ -1,26 +1,30 @@
 from utils.startup import * 
-import ast
+
+from covid.datasets.dataframes import (
+    LABEL_COLS, CAPTIAL_TO_SMALL_STUDY_LABEL, SMALL_TO_CAPITAL_STUDY_LABEL, DICOM_META_COLS, \
+    read_dataframes, 
+)
 
 # Competition Specific
 COMP_NAME = 'siim-covid19-detection'
 COMP_INPUT_PATH = Path('/kaggle/input/siim-covid19-detection')
 S3_PATH = 's3://siim-covid19-detection/'
 
-def download_from_s3(dataset_name, output_path):
-    output_path = str(output_path)
-    bucket_path = S3_PATH + dataset_name
-    subprocess.run(['aws', 's3', 'cp', bucket_path, output_path, '--recursive'], stdout=subprocess.PIPE)
-
-# Dataset Setup Functions / Utils
-from covid.datasets.dataframes import (
-    LABEL_COLS, CAPTIAL_TO_SMALL_STUDY_LABEL, SMALL_TO_CAPITAL_STUDY_LABEL, DICOM_META_COLS, \
-    read_dataframes, 
-)
 try: 
     dataframes_dir = Path('./dataframes')
     train, valid, test = read_dataframes(0, dataframes_dir)
 except Exception as e: 
     print(f'Exception reading dataframes: {e}')
+
+def download_from_s3(dataset_name, output_path):
+    output_path = str(output_path)
+    bucket_path = S3_PATH + dataset_name
+    subprocess.run(['aws', 's3', 'cp', bucket_path, output_path, '--recursive', '--quiet'], stdout=subprocess.PIPE)
+
+def upload_to_s3(folder_path, dataset_name):
+    folder_path = str(folder_path)
+    bucket = S3_PATH + dataset_name
+    subprocess.run(['aws', 's3', 'cp', folder_path, bucket, '--recursive', '--quiet'], stdout=subprocess.PIPE)
 
 def get_all_filepaths(data_dir):
     filepaths = glob.glob(str(data_dir / '*' / '**'), recursive=True) 
