04ee4dc8ef1ba65b4400e1596b7ab642898a6eec
Sarthak <sarthak.bhatt314@gmail.com>
2021-07-04 01:01:20 +0530

    lazy push

--- decimeter/startup.py
+++ decimeter/startup.py
@@ -1,4 +1,7 @@
 from utils.startup import * 
 
+
+# Competition Constants
+COMP_NAME = 'google-smartphone-decimeter-challenge'
 COMP_DRIVE_DIR = Path('/content/drive/MyDrive/Decimeter')
 COMP_DRIVE_DATA_DIR = COMP_DRIVE_DIR / 'Data'
