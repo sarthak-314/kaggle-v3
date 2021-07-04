42a274f8706c943eca132ce082e01bfd75cc05b7
Sarthak <sarthak.bhatt314@gmail.com>
2021-07-05 01:50:43 +0530

    lazy push

--- covid/dataframes.py
+++ covid/dataframes.py
@@ -124,7 +124,7 @@ def read_dataframes(fold=0, dataframes_dir=Path('./')):
             print('df_dir', df_dir)
         else: 
             print('no no', df_dir)
-    print(os.listdir(dataframes_dir))
-    train, valid = utils.dataframes.read_fold(fold, dataframes_dir, num_folds=NUM_FOLDS)
-    test = pd.read_pickle(dataframes_dir/'test.pkl')
-    return train, valid, test    
+    # print(os.listdir(dataframes_dir))
+    # train, valid = utils.dataframes.read_fold(fold, dataframes_dir, num_folds=NUM_FOLDS)
+    # test = pd.read_pickle(dataframes_dir/'test.pkl')
+    # return train, valid, test    
