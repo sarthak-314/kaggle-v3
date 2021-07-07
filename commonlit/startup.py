81aab8a67feebb7c5e24549072620508c2926a4b
Sarthak <sarthak.bhatt314@gmail.com>
2021-07-08 00:47:32 +0530

    lazy push

---
+++ commonlit/startup.py
@@ -0,0 +1,10 @@
+from utils.startup import * 
+from utils.utils import solve_environment
+
+# from mlb.dataframes import 
+
+# Competition Specific
+COMP_NAME = 'commonlitreadabilityprize'
+
+COMP_DRIVE_DIR = Path('/content/drive/MyDrive/Commonlit')
+COMP_DRIVE_DATA_DIR = Path('/content/drive/MyDrive/Commonlit/Data')
