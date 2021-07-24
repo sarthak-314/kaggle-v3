59f3328fbc1edd1ce0377f87453f2482b484f8c1
Sarthak <sarthak.bhatt314@gmail.com>
2021-07-25 01:34:15 +0530

    lazy push

--- covid/lightning/loader.py
+++ covid/lightning/loader.py
@@ -1,3 +1,4 @@
+
 import torch
 import os 
 
@@ -10,7 +11,7 @@ def create_loader(dataset, batch_size=32, is_training=False):
         shuffle=is_training, 
         drop_last=is_training, 
         num_workers=num_workers, 
-        pin_memory=torch.cuda.is_available(), 
-        persistent_workers=True,
+        # pin_memory=torch.cuda.is_available(), 
+        # persistent_workers=True,
     )
     return data_loader
