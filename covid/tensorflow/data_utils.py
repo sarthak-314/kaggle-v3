8ceb056d1cf5d3ee2388b7891026c4eb0c191772
Sarthak <sarthak.bhatt314@gmail.com>
2021-07-25 05:05:39 +0530

    lazy push

--- covid/tensorflow/data_utils.py
+++ covid/tensorflow/data_utils.py
@@ -4,7 +4,7 @@ def get_decode_fn(img_extension, channels):
     def decode_fn(path, label): 
         file_bytes = tf.io.read_file(path)
         if img_extension == 'png':
-            img = tf.image.decode_image(file_bytes, channels=channels)
+            img = tf.io.decode_image(file_bytes, channels=channels)
         elif img_extension in ['jpg', 'jpeg']:
             img = tf.image.decode_jpeg(file_bytes, channels=channels)
         img = tf.cast(img, tf.float32) / 255.0
