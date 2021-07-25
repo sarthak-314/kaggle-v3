9001338c835b0443a34f199c18d0ad699060005c
Sarthak <sarthak.bhatt314@gmail.com>
2021-07-25 08:58:47 +0530

    lazy push

--- covid/tensorflow/aug_tf.py
+++ covid/tensorflow/aug_tf.py
@@ -163,15 +163,15 @@ def train_img_augment(img, label, crop_percentage, img_size, channels):
     
     if p_cutout < 0.25: 
         mask_size = tf.random.uniform([], 0, 4, dtype=tf.int32)
-        mask = tf.Tensor(mask_size, mask_size, dtype=tf.int32)
+        mask = (mask_size, mask_size)
         img = tfa.image.random_cutout(img, mask , 0)
     if p_cutout < 0.5:
         mask_size = tf.random.uniform([], 0, 8, dtype=tf.int32)
-        mask = tf.Tensor(mask_size, mask_size, dtype=tf.int32)
+        mask = (mask_size, mask_size)
         img = tfa.image.random_cutout(img, mask,  0)
     if p_cutout < 1: 
         mask_size = tf.random.uniform([], 0, 16, dtype=tf.int32)
-        mask = tf.Tensor(mask_size, mask_size, dtype=tf.int32)
+        mask = (mask_size, mask_size)
         img = tfa.image.random_cutout(img, mask, 0)
     img = tf.image.resize(img, size=[img_size, img_size])
     return img, label
