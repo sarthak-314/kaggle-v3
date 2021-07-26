4d86b5baabcb122caa6b949f7fe9d828f950fdc7
Sarthak <sarthak.bhatt314@gmail.com>
2021-07-27 05:17:16 +0530

    lazy push

--- covid/tensorflow/lr.py
+++ covid/tensorflow/lr.py
@@ -203,3 +203,86 @@ class WarmUpCosineDecayScheduler(keras.callbacks.Callback):
         if self.verbose > 0:
             if batch < 10 or batch // 64 == 0: 
                 print('\nBatch %05d: setting lr to %s.' % (self.global_step + 1, lr))
+
+
+from tensorflow.python.keras.optimizer_v2 import *
+@keras_export("keras.optimizers.schedules.CosineDecayRestarts")
+class CosineDecayRestarts(LearningRateSchedule):
+    """A LearningRateSchedule that uses a cosine decay schedule with restarts.
+    See [Loshchilov & Hutter, ICLR2016](https://arxiv.org/abs/1608.03983),
+    SGDR: Stochastic Gradient Descent with Warm Restarts.
+    When training a model, it is often useful to lower the learning rate as
+    the training progresses. This schedule applies a cosine decay function with
+    restarts to an optimizer step, given a provided initial learning rate.
+    It requires a `step` value to compute the decayed learning rate. You can
+    just pass a TensorFlow variable that you increment at each training step.
+    The schedule a 1-arg callable that produces a decayed learning
+    rate when passed the current optimizer step. This can be useful for changing
+    the learning rate value across different invocations of optimizer functions.
+    The learning rate multiplier first decays
+    from 1 to `alpha` for `first_decay_steps` steps. Then, a warm
+    restart is performed. Each new warm restart runs for `t_mul` times more
+    steps and with `m_mul` times smaller initial learning rate.
+    Example usage:
+    ```python
+        first_decay_steps = 1000
+        lr_decayed_fn = (
+        tf.keras.optimizers.schedules.CosineDecayRestarts(
+            initial_learning_rate,
+            first_decay_steps))
+    ```
+    You can pass this schedule directly into a `tf.keras.optimizers.Optimizer`
+    as the learning rate. The learning rate schedule is also serializable and
+    deserializable using `tf.keras.optimizers.schedules.serialize` and
+    `tf.keras.optimizers.schedules.deserialize`.
+    Returns:
+    A 1-arg callable learning rate schedule that takes the current optimizer
+    step and outputs the decayed learning rate, a scalar `Tensor` of the same
+    type as `initial_learning_rate`.
+    """
+    def __init__(self, initial_learning_rate, first_decay_steps, t_mul=2.0, m_mul=1.0, alpha=0, name=None): 
+        super(CosineDecayRestarts, self).__init__()
+        self.initial_learning_rate = initial_learning_rate
+        self.first_decay_steps = first_decay_steps
+        self._t_mul = t_mul
+        self._m_mul = m_mul
+        self.alpha = alpha
+        self.name = name
+
+    def __call__(self, step): 
+        if step < 128: 
+            return 1e-6
+        with ops.name_scope_v2(self.name or 'SGDRDecay' as name:
+            initial_learning_rate = ops.convert_to_tensor_v2_with_dispatch(
+                self.initial_learning_rate, name="initial_learning_rate")
+        dtype = initial_learning_rate.dtype
+        first_decay_steps = math_ops.cast(self.first_decay_steps, dtype)
+        alpha = math_ops.cast(self.alpha, dtype)
+        t_mul = math_ops.cast(self._t_mul, dtype)
+        m_mul = math_ops.cast(self._m_mul, dtype)
+        global_step_recomp = math_ops.cast(step, dtype)
+        completed_fraction = global_step_recomp / first_decay_steps
+    
+        def compute_step(completed_fraction, geometric=False):
+            """Helper for `cond` operation."""
+            if geometric:
+                i_restart = math_ops.floor(
+                    math_ops.log(1.0 - completed_fraction * (1.0 - t_mul)) /
+                    math_ops.log(t_mul))
+
+                sum_r = (1.0 - t_mul**i_restart) / (1.0 - t_mul)
+                completed_fraction = (completed_fraction - sum_r) / t_mul**i_restart
+            else:
+                i_restart = math_ops.floor(completed_fraction)
+                completed_fraction -= i_restart
+            return i_restart, completed_fraction
+
+    def get_config(self):
+        return {
+            "initial_learning_rate": self.initial_learning_rate,
+            "first_decay_steps": self.first_decay_steps,
+            "t_mul": self._t_mul,
+            "m_mul": self._m_mul,
+            "alpha": self.alpha,
+            "name": self.name
+        }
