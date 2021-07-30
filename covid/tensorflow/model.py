0702bf41afd7275d154f19399abc787848a09761
Sarthak <sarthak.bhatt314@gmail.com>
2021-07-30 22:35:33 +0530

    lazy push

--- covid/tensorflow/model.py
+++ covid/tensorflow/model.py
@@ -9,49 +9,30 @@ import os
 def get_save_locally(): 
     return tf.saved_model.SaveOptions(experimental_io_device='/job:localhost')
 
-def _get_ranger(ranger_kwargs): 
-    radam = tfa.optimizers.RectifiedAdam( 
-        lr=ranger_kwargs['lr'],
-        total_steps=ranger_kwargs['total_steps'], 
-        warmup_proportion=ranger_kwargs['warmup_proportion'], 
-        amsgrad=ranger_kwargs['amsgrad'],
-        min_lr=0,
-        weight_decay=1e-4, # default is 0
-        #clipnorm = 10, # Not present by befault
-        name='Ranger',
-    )
-    ranger = tfa.optimizers.Lookahead(radam, sync_period=6, slow_step_size=0.5)
-    return ranger
+def optimizer_factory(optimizer_args):
+    optimizer_name, lr = optimizer_args['optimizer'], optimizer_args['lr']
+    if optimizer_name.lower() == 'adam': 
+        return tf.keras.optimizers.Adam(lr)
+    elif optimizer_name.lower() == 'ranger': 
+        radam = tfa.optimizers.RectifiedAdam( 
+            lr=lr,
+            total_steps=optimizer_args['total_steps'], 
+            warmup_proportion=optimizer_args['warmup_proportion'], 
+            amsgrad=optimizer_args['amsgrad'],
+            min_lr=0,
+            weight_decay=1e-4, 
+            name='Ranger',
+        )
+        ranger = tfa.optimizers.Lookahead(radam, sync_period=6, slow_step_size=0.5)
+        return ranger
 
-def build_model(tfhub_url, z_dim, num_dense, dropout=0.5): 
-    start_time = time()
-    if z_dim is not 0:     
-        model = tf.keras.Sequential([
-            hub.KerasLayer(tfhub_url, trainable=True), 
-            tf.keras.layers.Dropout(dropout),
-            tf.keras.layers.Dense(
-                z_dim, 
-                activation=tfa.activations.mish, 
-                kernel_regularizer=tf.keras.regularizers.l2(1e-4)
-            ),
-            tf.keras.layers.Dropout(dropout), 
-            tf.keras.layers.Dense(num_dense, kernel_regularizer=tf.keras.regularizers.l2(1e-3)),
-        ])
-    else: 
-        model = tf.keras.Sequential([
-            hub.KerasLayer(tfhub_url, trainable=True), 
-            tf.keras.layers.Dropout(dropout), 
-            tf.keras.layers.Dense(num_dense, kernel_regularizer=tf.keras.regularizers.l2(1e-3)),
-        ])
-    print(f'{time()-start_time} seconds to build the model')
-    return model
 
-def compile_model(model, strategy, loss, metrics=['accuracy'], ranger_kwargs=None): 
+def compile_model(model, strategy, loss, metrics=['accuracy'], optimizer_args=None): 
     with strategy.scope(): 
         model.compile(
             loss=loss,
             metrics=metrics, 
-            optimizer=_get_ranger(ranger_kwargs), 
+            optimizer=optimizer_factory(optimizer_args), 
             steps_per_execution=64,
         )
     print('Model Compiled')
