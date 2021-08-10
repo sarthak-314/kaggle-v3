92a95c028658cf12df5b2ac5f9f7a22cc9736487
Sarthak <sarthak.bhatt314@gmail.com>
2021-08-10 11:58:38 +0530

    lazy push

---
+++ tumor/tensorflow/data.py
@@ -0,0 +1,168 @@
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+import tensorflow as tf
+from time import time 
+import pandas as pd
+
+from covid.tensorflow.augmentations.basic import get_basic_augmentations
+from covid.tensorflow.augmentations.cutmix_mixup import get_cutmix_mixup
+from covid.tensorflow.augmentations.gridmask import get_grid_mask
+from covid.tensorflow.augmentations.augmix import get_augmix
+
+def get_ignore_order(): 
+    ignore_order = tf.data.Options()
+    ignore_order.experimental_deterministic = False # disable order, increase speed
+    return ignore_order
+
+def build_dataset_v1(img_paths, labels, decode_fn, img_transforms=[], batch_transforms=[], batch_size=4, is_training=False):
+    ds = tf.data.Dataset.from_tensor_slices((img_paths, labels))
+    ds = ds.map(decode_fn, num_parallel_calls=tf.data.AUTOTUNE)
+    ds = ds.cache().with_options(get_ignore_order())
+    if is_training: 
+        ds = ds.repeat().shuffle(512)
+    for img_transform in img_transforms: 
+        ds = ds.map(img_transform, num_parallel_calls=tf.data.AUTOTUNE)
+    ds = ds.batch(batch_size, drop_remainder=True)
+    for batch_transform in batch_transforms: 
+        ds = ds.map(batch_transform, num_parallel_calls=tf.data.AUTOTUNE)
+    return ds.prefetch(tf.data.AUTOTUNE)
+
+
+
+def get_steps(df, batch_size): 
+    steps = len(df) // batch_size
+    return max(steps, 1)
+
+def get_decode_fn(img_extension, channels):
+    def decode_fn(path, label): 
+        file_bytes = tf.io.read_file(path)
+        if img_extension == 'png':
+            img = tf.image.decode_png(file_bytes, channels=channels)
+        elif img_extension in ['jpg', 'jpeg']:
+            img = tf.image.decode_jpeg(file_bytes, channels=channels)
+        img = tf.cast(img, tf.float32) / 255.0
+        return img, label
+    return decode_fn
+
+def get_img_transforms(img_transforms, img_size, aug_params): 
+    get_basic_augs, get_random_scale, get_random_rotate, get_random_cutout, get_random_blur, get_resize_fn = get_basic_augmentations()
+    transform_string_to_transform = {
+        'basic_augmentations': get_basic_augs(img_size), 
+        'random_scale' : get_random_scale(img_size, aug_params['scale']), 
+        'random_rotate' : get_random_rotate(img_size, aug_params['rot_prob']), 
+        'random_cutout' : get_random_cutout(img_size, aug_params['cutout']), 
+        'random_blur' : get_random_blur(aug_params['blur']), 
+        'resize' : get_resize_fn(img_size), 
+        'gridmask' : get_grid_mask(img_size, aug_params['gridmask']), 
+        'augmix': get_augmix(img_size, aug_params['augmix']),
+    }
+    return [transform_string_to_transform[transform_str] for transform_str in img_transforms]
+
+
+def get_batch_transforms(batch_transforms, img_size, aug_params, classes, batch_size): 
+    cutmix, mixup = get_cutmix_mixup(img_size, classes, batch_size, cutmix_prob=aug_params['cutmix_prob'], mixup_prob=aug_params['mixup_prob'])
+    transform_string_to_transform = {
+        'cutmix': cutmix, 
+        'mixup': mixup, 
+    }
+    return [transform_string_to_transform[transform_str] for transform_str in batch_transforms]
+
+
+def get_train_ds_fn(img_size, img_ext, num_classes, aug_params, img_transforms, batch_transforms): 
+    def get_train_ds(train, batch_size, debug_frac=1): 
+        # Load train DataFrame
+        if debug_frac != 1: 
+            print(f'Taking {debug_frac*100}% of train ({int(len(train)*debug_frac)} samples), with batch size: {batch_size}')
+            train = train.sample(frac=debug_frac)
+        train = train.sample(frac=1)
+        
+        # Build train dataset
+        start_time = time()
+        train_ds = build_dataset_v1(
+            img_paths=train.img_path.values, 
+            labels=pd.get_dummies(train.label).values, 
+            decode_fn=get_decode_fn(img_ext, channels=3), 
+            img_transforms=get_img_transforms(img_transforms, img_size, aug_params), 
+            batch_transforms=get_batch_transforms(batch_transforms, img_size, aug_params, num_classes, batch_size), 
+            batch_size=batch_size, 
+            is_training=False, 
+        )
+        print(f'{time()-start_time} seconds to load train_ds')
+        
+        # train_steps 
+        train_steps = get_steps(train, batch_size)
+        print(f'{(time()-start_time)*(train_steps/60)} minutes, {train_steps} steps for the first epoch ')
+        return train_ds, train_steps
+    return get_train_ds
+
+
+def get_clean_ds_fn(img_size, img_ext, aug_params, img_transforms=['resize']):
+    def get_clean_ds(df, batch_size, debug_frac=1): 
+        # Load the dataframe
+        if debug_frac != 1: 
+            df = df.sample(frac=debug_frac)
+        df = df.sample(frac=1)
+        
+        # Load the dataset 
+        start_time = time()
+        clean_ds = build_dataset_v1(
+            img_paths=df.img_path.values, 
+            labels=pd.get_dummies(df.label).values, 
+            decode_fn= get_decode_fn(img_ext, channels=3), 
+            img_transforms=get_img_transforms(img_transforms, img_size, aug_params), 
+            batch_transforms=[], 
+            batch_size=batch_size, 
+            is_training=False, 
+        )
+        print(f'{time()-start_time} seconds to load clean ds')
+        
+        return clean_ds, get_steps(df, batch_size)
+    return get_clean_ds
+
+
+def visualize_augmentations(should_visualize, train_ds, rows, cols): 
+    if not should_visualize: 
+        print('Skipping visualization')
+        return 
+    fig = plt.figure(figsize=(32, 12))
+    num_imgs = rows * cols
+    for i, x in tqdm(enumerate(train_ds.unbatch().take(num_imgs)), total=num_imgs): 
+        img = x[0]
+        fig.add_subplot(rows, cols, i+1)
+        plt.imshow(img)
+
+def build_static_hash_table(input_dict, default_value=-1):
+    keys = list(input_dict.keys()) 
+    values = list(input_dict.values())
+    table = tf.lookup.StaticHashTable(
+            initializer=tf.lookup.KeyValueTensorInitializer(
+                keys=tf.constant(list(keys)),
+                values=tf.constant(list(values)), 
+            ), 
+            default_value=default_value,
+        )
+    return table
+        
+def get_encode_label(labels): 
+    'Convert labels to number than one hot encode them'
+    label2idx = {label: idx for idx, label in enumerate(labels)}
+    keys = list(label2idx.keys())
+    values = list(label2idx.values())
+    def encode_label(img, label): 
+        table = tf.lookup.StaticHashTable(
+            initializer=tf.lookup.KeyValueTensorInitializer(
+                keys=tf.constant(keys),
+                values=tf.constant(values), 
+            ), 
+            default_value=-1,
+        )
+        label = table.lookup(label)
+        label = tf.cast(label, tf.uint8)
+        label = tf.one_hot(label, len(labels))
+        return img, label
+    return encode_label
+
+
+def img_as_float(img, label): 
+    img = tf.cast(img, tf.float32) / 255.0
+    return img, label
---
+++ tumor/tensorflow/startup.py
@@ -0,0 +1,75 @@
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
+import tensorflow_addons as tfa
+import tensorflow_hub as hub
+import tensorflow as tf
+
+# Full Module Imports 
+from covid.tensorflow.callbacks import * 
+from covid.tensorflow.tfrecord import * 
+from covid.tensorflow.model import *
+from covid.tensorflow.data import * 
+from covid.tensorflow.lr import *
+
+# Function / Classes Imports
+from covid.tensorflow.model import save_model
+
+def auto_select_accelerator():
+    try:
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+        tf.config.experimental_connect_to_cluster(tpu)
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        strategy = tf.distribute.TPUStrategy(tpu)
+        print("Running on TPU:", tpu.master())
+    except ValueError:
+        strategy = tf.distribute.get_strategy()
+    print(f"Running on {strategy.num_replicas_in_sync} replicas")
+    return strategy
+
+def get_gcs_path(dataset_name): 
+    from kaggle_datasets import KaggleDatasets
+    return KaggleDatasets().get_gcs_path(dataset_name)
+
+def get_ranger(lr, min_lr=0): 
+    radam = tfa.optimizers.RectifiedAdam( 
+        learning_rate = lr,
+        min_lr=min_lr,
+        weight_decay=1e-4, # default is 0
+        amsgrad = True,
+        name = 'Ranger',
+        #clipnorm = 10, # Not present by befault
+    )
+    return radam
+
+def get_save_locally(): 
+    return tf.saved_model.SaveOptions(experimental_io_device='/job:localhost')
+
+def get_load_locally(): 
+    return tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
+
+def decode_fn(path, ext): 
+    file_bytes = tf.io.read_file(path)
+    if ext == 'png':
+        img = tf.image.decode_png(file_bytes, channels=3)
+    elif ext in ['jpg', 'jpeg']:
+        img = tf.image.decode_jpeg(file_bytes, channels=3)
+    img = tf.cast(img, tf.float32) / 255.0
+    return img
+
+def resize_img(img, img_size): 
+    img = tf.image.resize(img, (img_size, img_size))
+    return img
+
+def augment_fn(img):
+    # TODO: Look More Into Augmentations 
+    img = tf.image.random_flip_left_right(img)
+    img = tf.image.random_flip_up_down(img)
+    return img
+
+def enable_mixed_precision(): 
+    policy = tf.keras.mixed_precision.experimental.Policy('mixed_bfloat16')
+    mixed_precision.set_policy(policy)
+    print('Mixed precision enabled')
+
+def enable_jit(): 
+    tf.config.optimizer.set_jit(True)
+    print('Accelerated Linear Algebra enabled')
