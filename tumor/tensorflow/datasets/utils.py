d43cb15ae7275122f871f2c5f9355e7d6a31a3bc
Sarthak <sarthak.bhatt314@gmail.com>
2021-08-16 06:00:15 +0530

    lazy push

--- tumor/tensorflow/datasets/utils.py
+++ tumor/tensorflow/datasets/utils.py
@@ -6,3 +6,7 @@ def get_ignore_order():
     ignore_order = tf.data.Options()
     ignore_order.experimental_deterministic = False # disable order, increase speed
     return ignore_order
+
+def get_gcs_path(dataset_name): 
+    from kaggle_datasets import KaggleDatasets
+    return KaggleDatasets().get_gcs_path(dataset_name)
