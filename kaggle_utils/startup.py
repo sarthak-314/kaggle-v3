f9aacdcddf19917c66326d766061ea47d8be9814
Sarthak <sarthak.bhatt314@gmail.com>
2021-08-20 14:59:51 +0530

    lazy push

--- kaggle_utils/startup.py
+++ kaggle_utils/startup.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass, asdict
 from distutils.dir_util import copy_tree
 from collections import defaultdict
 import matplotlib.pyplot as plt
+from functools import partial
 from termcolor import colored
 from tqdm.auto import tqdm
 from pathlib import Path 
