a48dd636ca5f1674ffff0ab8f555807a68a42347
Sarthak <sarthak.bhatt314@gmail.com>
2021-08-24 03:48:00 +0530

    lazy push

--- chai/tensorflow/callbacks.py
+++ chai/tensorflow/callbacks.py
@@ -38,6 +38,7 @@ def tb_callback(train_steps):
 def model_checkpoint(checkpoint_dir=None):
     checkpoint_filepath = 'checkpoint-{epoch:02d}-{val_loss:.4f}.h5'
     if checkpoint_dir is not None: 
+        os.makedirs(checkpoint_dir, exist_ok=True)
         checkpoint_filepath = checkpoint_dir / checkpoint_filepath
     return tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
--- chai/tensorflow/startup.py
+++ chai/tensorflow/startup.py
@@ -99,7 +99,7 @@ def tf_lr_scheduler_factory(lr_scheduler_kwargs):
 def tf_optimizer_factory(optimizer_kwargs, lr_scheduler): 
     optimizer_name = optimizer_kwargs.pop('name')
     if optimizer_name == 'AdamW': 
-        optimizer = tfa.optimizer.AdamW(
+        optimizer = tfa.optimizers.AdamW(
             weight_decay=optimizer_kwargs['weight_decay'],
             learning_rate=lr_scheduler,  
             amsgrad=optimizer_kwargs['use_amsgrad'], 
